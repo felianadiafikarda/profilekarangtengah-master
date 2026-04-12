238b15346484b7ae5304caa4c6c7414b02fdc19c
felianadiafikarda <139899021+felianadiafikarda@users.noreply.github.com>
2026-04-12 17:18:56 +0000

    route api

--- app/api/fasilitas/upload/route.ts
+++ app/api/fasilitas/upload/route.ts
@@ -5,11 +5,7 @@ import { existsSync } from "fs";
 import path from "path";
 import { randomUUID } from "crypto";
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+export const dynamic = 'force-dynamic';
 
 export async function POST(req: NextRequest) {
   try {
